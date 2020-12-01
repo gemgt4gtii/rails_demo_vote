4663374e9642f7f179ef5fadeeb68e03e2565120
gemgt4gtii <z123456618@hotmail.com>
2020-12-01 09:18:14 +0800

    Script upload

---
+++ git_autopush.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "git add ."
+git add .
+
+echo "git commit"
+echo "请输入commit的注释信息:"
+comment="commit new code"
+read comment
+git commit -m "$comment"
+
+echo "git fetch origin master"
+git fetch origin master
+
+echo "git merge origin/master"
+git merge origin/master
+
+echo "git push origin master:master"
+git push origin master:master
